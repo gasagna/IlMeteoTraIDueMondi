300251e8fea338d64e7d179f96739a98176cc7d8
Davide Lasagna <lasagnadavide@gmail.com>
2024-09-24 21:54:19 +0100

    update style

--- public/index.js
+++ public/index.js
@@ -27,56 +27,56 @@ function make_ultima_misurazione(date) {
 fetch("data/last_measurement_introd.json")
 	.then(response => response.json())
 	.then(data => {
-		document.getElementById("temperatura_corrente").innerHTML    = data["temperature"] + " °C";
-		document.getElementById("umidità_corrente").innerHTML        = data["humidity"] + " %";
-		document.getElementById("pressione_corrente").innerHTML      = data["pressure"] + " hPa";
-		document.getElementById("data_ultima_rilevazione").innerHTML = make_ultima_misurazione(data["_time"]);
+		document.getElementById("temperatura_corrente_introd").innerHTML    = data["temperature"] + " °C";
+		document.getElementById("umidità_corrente_introd").innerHTML        = data["humidity"] + " %";
+		document.getElementById("pressione_corrente_introd").innerHTML      = data["pressure"] + " hPa";
+		document.getElementById("data_ultima_rilevazione_introd").innerHTML = make_ultima_misurazione(data["_time"]);
 	})
 
 // EXTREMA TODAY
 fetch("data/extrema_today_introd.json")
 	.then(response => response.json())
 	.then(data => {
-		document.getElementById("min_temperature_today").innerHTML = data["min_temperature"] + " °C alle " + make_time(data["min_temperature_time"]);
-		document.getElementById("max_temperature_today").innerHTML = data["max_temperature"] + " °C alle " + make_time(data["max_temperature_time"]);
-		document.getElementById("min_humidity_today").innerHTML = data["min_humidity"] + " % alle " + make_time(data["min_humidity_time"]);
-		document.getElementById("max_humidity_today").innerHTML = data["max_humidity"] + " % alle " + make_time(data["max_humidity_time"]);
-		document.getElementById("min_pressure_today").innerHTML = data["min_pressure"] + " hPa alle " + make_time(data["min_pressure_time"]);
-		document.getElementById("max_pressure_today").innerHTML = data["max_pressure"] + " hPa alle " + make_time(data["max_pressure_time"]);
+		document.getElementById("min_temperature_today_introd").innerHTML = data["min_temperature"] + " °C alle " + make_time(data["min_temperature_time"]);
+		document.getElementById("max_temperature_today_introd").innerHTML = data["max_temperature"] + " °C alle " + make_time(data["max_temperature_time"]);
+		document.getElementById("min_humidity_today_introd").innerHTML = data["min_humidity"] + " % alle " + make_time(data["min_humidity_time"]);
+		document.getElementById("max_humidity_today_introd").innerHTML = data["max_humidity"] + " % alle " + make_time(data["max_humidity_time"]);
+		document.getElementById("min_pressure_today_introd").innerHTML = data["min_pressure"] + " hPa alle " + make_time(data["min_pressure_time"]);
+		document.getElementById("max_pressure_today_introd").innerHTML = data["max_pressure"] + " hPa alle " + make_time(data["max_pressure_time"]);
 	})
 
 // EXTREMA YESTERDAY
 fetch("data/extrema_yesterday_introd.json")
 	.then(response => response.json())
 	.then(data => {
-		document.getElementById("min_temperature_yesterday").innerHTML = data["min_temperature"] + " °C alle " + make_time(data["min_temperature_time"]);
-		document.getElementById("max_temperature_yesterday").innerHTML = data["max_temperature"] + " °C alle " + make_time(data["max_temperature_time"]);
-		document.getElementById("min_humidity_yesterday").innerHTML = data["min_humidity"] + " % alle " + make_time(data["min_humidity_time"]);
-		document.getElementById("max_humidity_yesterday").innerHTML = data["max_humidity"] + " % alle " + make_time(data["max_humidity_time"]);
-        document.getElementById("min_pressure_yesterday").innerHTML = data["min_pressure"] + " hPa alle " + make_time(data["min_pressure_time"]);
-		document.getElementById("max_pressure_yesterday").innerHTML = data["max_pressure"] + " hPa alle " + make_time(data["max_pressure_time"]);
+		document.getElementById("min_temperature_yesterday_introd").innerHTML = data["min_temperature"] + " °C alle " + make_time(data["min_temperature_time"]);
+		document.getElementById("max_temperature_yesterday_introd").innerHTML = data["max_temperature"] + " °C alle " + make_time(data["max_temperature_time"]);
+		document.getElementById("min_humidity_yesterday_introd").innerHTML = data["min_humidity"] + " % alle " + make_time(data["min_humidity_time"]);
+		document.getElementById("max_humidity_yesterday_introd").innerHTML = data["max_humidity"] + " % alle " + make_time(data["max_humidity_time"]);
+        document.getElementById("min_pressure_yesterday_introd").innerHTML = data["min_pressure"] + " hPa alle " + make_time(data["min_pressure_time"]);
+		document.getElementById("max_pressure_yesterday_introd").innerHTML = data["max_pressure"] + " hPa alle " + make_time(data["max_pressure_time"]);
 	})
 
 // EXTREMA THIS MONTH
 fetch("data/extrema_this_month_introd.json")
 	.then(response => response.json())
 	.then(data => {
-		document.getElementById("min_temperature_this_month").innerHTML = data["min_temperature"] + " °C il " + make_short_date(data["min_temperature_time"]);
-		document.getElementById("max_temperature_this_month").innerHTML = data["max_temperature"] + " °C il " + make_short_date(data["max_temperature_time"]);
-		document.getElementById("min_humidity_this_month").innerHTML = data["min_humidity"] + " % il " + make_short_date(data["min_humidity_time"]);
-		document.getElementById("max_humidity_this_month").innerHTML = data["max_humidity"] + " % il " + make_short_date(data["max_humidity_time"]);
-        document.getElementById("min_pressure_this_month").innerHTML = data["min_pressure"] + " hPa il " + make_short_date(data["min_pressure_time"]);
-		document.getElementById("max_pressure_this_month").innerHTML = data["max_pressure"] + " hPa il " + make_short_date(data["max_pressure_time"]);
+		document.getElementById("min_temperature_this_month_introd").innerHTML = data["min_temperature"] + " °C il " + make_short_date(data["min_temperature_time"]);
+		document.getElementById("max_temperature_this_month_introd").innerHTML = data["max_temperature"] + " °C il " + make_short_date(data["max_temperature_time"]);
+		document.getElementById("min_humidity_this_month_introd").innerHTML = data["min_humidity"] + " % il " + make_short_date(data["min_humidity_time"]);
+		document.getElementById("max_humidity_this_month_introd").innerHTML = data["max_humidity"] + " % il " + make_short_date(data["max_humidity_time"]);
+        document.getElementById("min_pressure_this_month_introd").innerHTML = data["min_pressure"] + " hPa il " + make_short_date(data["min_pressure_time"]);
+		document.getElementById("max_pressure_this_month_introd").innerHTML = data["max_pressure"] + " hPa il " + make_short_date(data["max_pressure_time"]);
 	})
 
 // EXTREMA LAST MONTH
 fetch("data/extrema_last_month_introd.json")
 	.then(response => response.json())
 	.then(data => {
-		document.getElementById("min_temperature_last_month").innerHTML = data["min_temperature"] + " °C il " + make_short_date(data["min_temperature_time"]);
-		document.getElementById("max_temperature_last_month").innerHTML = data["max_temperature"] + " °C il " + make_short_date(data["max_temperature_time"]);
-		document.getElementById("min_humidity_last_month").innerHTML = data["min_humidity"] + " % il " + make_short_date(data["min_humidity_time"]);
-		document.getElementById("max_humidity_last_month").innerHTML = data["max_humidity"] + " % il " + make_short_date(data["max_humidity_time"]);
-        document.getElementById("min_pressure_last_month").innerHTML = data["min_pressure"] + " hPa il " + make_short_date(data["min_pressure_time"]);
-		document.getElementById("max_pressure_last_month").innerHTML = data["max_pressure"] + " hPa il " + make_short_date(data["max_pressure_time"]);
+		document.getElementById("min_temperature_last_month_introd").innerHTML = data["min_temperature"] + " °C il " + make_short_date(data["min_temperature_time"]);
+		document.getElementById("max_temperature_last_month_introd").innerHTML = data["max_temperature"] + " °C il " + make_short_date(data["max_temperature_time"]);
+		document.getElementById("min_humidity_last_month_introd").innerHTML = data["min_humidity"] + " % il " + make_short_date(data["min_humidity_time"]);
+		document.getElementById("max_humidity_last_month_introd").innerHTML = data["max_humidity"] + " % il " + make_short_date(data["max_humidity_time"]);
+        document.getElementById("min_pressure_last_month_introd").innerHTML = data["min_pressure"] + " hPa il " + make_short_date(data["min_pressure_time"]);
+		document.getElementById("max_pressure_last_month_introd").innerHTML = data["max_pressure"] + " hPa il " + make_short_date(data["max_pressure_time"]);
 	})
